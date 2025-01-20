38afc968a8bcb830ddfd1a8cfa0b3c216df58ef7
野口瑠星 <noguchi.ryusei@k-idea.jp>
2025-01-20 14:40:05 +0900

    認証できた

--- ngryu-project/src/App.tsx
+++ ngryu-project/src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports.js';
+Amplify.configure(awsExports);
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
 const App: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
@@ -32,4 +36,4 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
---
+++ ngryu-project/src/aws-exports.js
@@ -0,0 +1,27 @@
+const awsmobile = {
+    "aws_project_region": "ap-northeast-1",
+    "aws_cognito_identity_pool_id": "ap-northeast-1:9b1b3ece-fdbf-45ae-8545-91e091fac57b",
+    "aws_cognito_region": "ap-northeast-1",
+    "aws_user_pools_id": "ap-northeast-1_R7pvvz4RN",
+    "aws_user_pools_web_client_id": "u38vp3lpddctdopusnj5aok9",
+    "oauth": {},
+    "aws_cognito_username_attributes": [],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+
+export default awsmobile;
